4865b37cd127204f5de2bd54cd99a41140136dbb
Neel Gupta <neelgupta2@gmail.com>
2016-12-15 17:25:49 -0800

    Add hidden X axis (in comment)

--- circlePacking.js
+++ circlePacking.js
@@ -8,31 +8,45 @@ d3.csv(data)
 			year: d['Year'],
 			county: d['County'],
 			description: d['Pediatric Quality Indicator Description'],
-			totalPop: Number(d['Population']),
+			totalPop: d['Population'],
 			count: Number(d['Count'])
 		}
 	})
 	.get((error, data) => {
 		if (error) throw error;
 
-		data = data.filter((d) => {
-			return d.county === 'STATEWIDE' && d.year === '2014';
-		})
+		data = data.filter((d) => { return d.county === 'STATEWIDE' && d.year === '2014'; })
 		console.log(data)
+
+	// const x = d3.scaleBand()
+	// 				.domain(data)
+	// 				.range(0, 170)
+	// 				.paddingInner(0.1176)
+
+	// const xAxis = d3.axisBottom(x)
+
+	// svg.append("g")
+	//       .attr("class","x axis hidden")
+	//       .attr("transform","translate(0,300)")
+	//       .call(xAxis)
+
 
 	const svg = d3.select('body').append('svg').attr('height', '100%').attr('width', '100%')
 
 	var newX = 200;
+	var newY;
+
+
 	svg.selectAll('circle.first')
 			.data(data)
 			.enter().append('circle')
-						.attr('class', 'first')
-						.attr('cx', (d, i) => {
-							newX += d.year / 10;
-							return newX;
-						})
-						.attr('cy', '50')
-						.attr('r', (d) => { return d.totalPop / 100000; })
+				.attr('class', 'first')
+				.attr('cx', (d) => {
+					newX += d.year / 1000;
+					return newX;
+				})
+				.attr('cy', '50')
+				.attr('r', (d) => { return d.totalPop / 100000; })
 
 
 })
